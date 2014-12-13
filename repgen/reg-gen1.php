a5868488b6b1945850192fb996fa48c640256a88
kausthub-sekar <kausthub.s@gmail.com>
2014-12-13 12:58:10 +0530

    Add new Xml2Pdf files

--- repgen/reg-gen1.php
+++ repgen/reg-gen1.php
@@ -1,16 +1,18 @@
 <?php
-require_once 'Xml2Pdf.php';
-/**
- * include config file.
- */
-require_once 'Xml2Pdf.config.php';
-
+//include the Xml2Pdf class
+require_once '../../classes/Xml2Pdf.php';
 /**
  * include the class Pdf.
 */
-require_once 'Pdf.php';
+require_once '../../classesPdf.php';
+//include the appvigil-report-parser
 require_once 'appvigil-report-parser.php';
 
+$log=new KLogger ( "/home/ubuntu/www/logs/$systemDate-Appvigil-exec-log.txt" , KLogger::ERROR );
 
+//custom user preferences will come from the appvigil-report-parser command line args
+$obj = new Xml2Pdf('report.xml');
+$pdf = $obj->render();
+$pdf->Output();
 
 ?>
