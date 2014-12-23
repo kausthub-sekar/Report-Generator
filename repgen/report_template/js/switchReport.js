ef2bed38d5a6119953cdc81444c5573632c11f7c
kausthub-sekar <kausthub.s@gmail.com>
2014-12-23 19:49:24 +0530

    Start wireframe based HTML report

--- repgen/report_template/js/switchReport.js
+++ repgen/report_template/js/switchReport.js
@@ -5,12 +5,24 @@ var j=$.noConflict(true);
  * switches HTML Report view back and forth between Developer and Executive report
  */
 j(document).ready(function() {
-		j('button').click(function() {
-			j('#MCode').slideToggle("fast",function() {
-				//hide or show  Malicious Code section
-			});
-			j('#hideMCode').slideToggle("fast",function() {
-				//hide or show the Malicious Code left hand menu item
-			});
+		j('form input:radio').click(function() {
+			if(j('input:checked').val()=="dev")
+				{
+				/*
+				 * this entire section's switching logic
+				 * will have to change based on the new wireframe mockup
+				 * maybe use show function to show all the details	
+				j('#MCode').slideToggle("fast",function() {
+					//hide or show  Malicious Code section
+				});
+				j('#hideMCode').slideToggle("fast",function() {
+					//hide or show the Malicious Code left hand menu item
+				});
+				*/
+				}
+			else
+				{
+				//hide all the detailed sections using the hide function
+				}
 		});
 });
